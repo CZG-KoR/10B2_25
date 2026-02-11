71486769a05a568c1038f4900d41f4f9a8641584
fwondraczek <fjowo@posteo.de>
2026-02-11 18:03:38 +0100

    erstellen von items nach vorlagen ermöglicht und umstrukturierung beim interagieren mit lagern

--- src/main/java/gameObject/Interactable.java
+++ src/main/java/gameObject/Interactable.java
@@ -2,11 +2,12 @@ package gameObject;
 
 public interface Interactable {
 
+
+    public abstract GameObjects interact(GameObjects gameObjects);
     /**
      * interaction for a Button where the Coordinate of the mouse is irrelevant
      * @param button clicked button
      */
-    public abstract GameObjects interact(GameObjects gameObjects);
     public abstract GameObjects interact(GameObjects gameObjects,int button);
     public abstract GameObjects interact(GameObjects gameObjects,int button,int xPosObjekt,int yPosObjekt,int xMouse,int yMouse);
 }
